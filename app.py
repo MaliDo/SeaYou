dbe482d8ad6513f1e92e07e4f1c33d622d712f13
Sergey Malivanov <sergey.m@knowledgecity.pro>
2025-04-06 01:55:16 +0300

    Like/unlike feature

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-from crypt import methods
-from http import client
-import flask
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, TextAreaField
-from flask import Flask, render_template, request, url_for, redirect, flash
-from flask_login import LoginManager, current_user, login_user, login_required, logout_user
-from flask_httpauth import HTTPBasicAuth
-from werkzeug.security import generate_password_hash, check_password_hash
-from data import offers
+# Standard libs
 from datetime import datetime
-import database
 from urllib.parse import urlparse, urljoin
+import os
+
+# Third-party
+import flask
+from flask import Flask, abort, flash, redirect, render_template, request, url_for, jsonify
+from flask_login import LoginManager, current_user, login_user, login_required, logout_user, UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, TextAreaField, IntegerField
+from flask_wtf.file import FileField, FileAllowed
+from wtforms.validators import DataRequired
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
+
+# Local
+import database
+from data import offers
 
 app = Flask(__name__)
 login_manager = LoginManager()
@@ -53,12 +59,27 @@ class SignUpForm(FlaskForm):
     email = StringField('Email')
     password_hash = PasswordField('Password')
     bio = TextAreaField('About info')
+    profile_picture = FileField('Profile Picture', validators=[
+        FileAllowed(['jpg', 'jpeg', 'png'], 'Images only!')
+    ])
     submit = SubmitField('Submit')
 
 class LoginForm(FlaskForm):
     username = StringField('Username')
     password = PasswordField('Password')
     submit = SubmitField('Submit')
+    
+class NewOfferForm(FlaskForm):
+    port_departure = StringField("Port of Departure", validators=[DataRequired()])
+    port_arrival = StringField("Port of Arrival", validators=[DataRequired()])
+    date_departure = StringField("Date of Departure (YYYY-MM-DD)", validators=[DataRequired()])
+    days = IntegerField("Number of Days", validators=[DataRequired()])
+    pax = IntegerField("Number of Guests (PAX)", validators=[DataRequired()])
+    price = IntegerField("Price (USD)", validators=[DataRequired()])
+    picture = FileField("Boat Picture", validators=[
+        FileAllowed(['jpg', 'jpeg', 'png'], 'Images only!')
+    ])
+    submit = SubmitField("Create Offer")
 
 def is_safe_url(target):
     ref_url = urlparse(request.host_url)
@@ -74,30 +95,24 @@ def main():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # Here we use a class of some kind to represent and validate our
-    # client-side form data. For example, WTForms is a library that will
-    # handle this for us, and we use a custom LoginForm to validate.
     form = LoginForm()
     if form.validate_on_submit():
-        # Login and validate the user.
         username = form.username.data
         password = form.password.data
 
         client = database.get_client_by_username(username)
 
-        if client and \
-            check_password_hash(client['PasswordHash'], password):
+        if client and check_password_hash(client['PasswordHash'], password):
             user = User(username)
-        # user should be an instance of your `User` class
             login_user(user)
-
+            
             next = flask.request.args.get('next')
-            # is_safe_url should check if the url is safe for redirects.
-            # See http://flask.pocoo.org/snippets/62/ for an example.
-            if not is_safe_url(next):
-                return flask.abort(400)
+            if not next or not is_safe_url(next):
+                next = flask.url_for('feed')
+
             flash('Logged in successfully.')
-            return redirect(next or flask.url_for('feed'))
+            return redirect(next)
+
     title = 'SeaYou - LogIn'
     user = current_user
     return flask.render_template('login.html', form=form, title=title, users=users, user=user)
@@ -106,7 +121,6 @@ def login():
 def signup():
     form = SignUpForm()
     if form.validate_on_submit():
-        # Login and validate the user.
         first_name = form.first_name.data
         last_name = form.last_name.data
         username = form.username.data
@@ -114,7 +128,14 @@ def signup():
         email = form.email.data
         password_hash = generate_password_hash(form.password_hash.data)
         bio = form.bio.data
-        database.create_user(first_name, last_name, username, dob, email, password_hash, bio)
+
+        profile_picture_filename = None
+        if form.profile_picture.data:
+            filename = secure_filename(form.profile_picture.data.filename)
+            profile_picture_filename = filename
+            form.profile_picture.data.save(os.path.join('static/ProfilePics', filename))
+
+        database.create_user(first_name, last_name, username, dob, email, password_hash, bio, profile_picture_filename)
         new_user = User(username)
         login_user(new_user)
         next = flask.request.args.get('next')
@@ -122,6 +143,7 @@ def signup():
             return flask.abort(400)
         flash('Logged in successfully.')
         return redirect(next or flask.url_for('feed'))
+
     title = 'SeaYou - SignUp'
     user = current_user
     return render_template('signup.html', title=title, form=form, user=user)
@@ -135,18 +157,40 @@ def logout():
 @app.route("/feed")
 @login_required
 def feed():
-    title = 'SeaYou - My Feed'
-    offers = database.get_offers()
-    user = current_user
-    return render_template('feed.html', title=title, offers=offers, user=user)
+	title = 'SeaYou - My Feed'
+	user_id = database.get_user_id(current_user.username)
+	offers = database.get_offers_for_user(user_id)
+	return render_template("feed.html", title=title, offers=offers, user=current_user)
 
-@app.route("/new")
+@app.route("/new", methods=['GET', 'POST'])
 @login_required
 def new():
+    form = NewOfferForm()
     title = 'SeaYou - New Offer'
-    offers = database.get_offers()
     user = current_user
-    return render_template('new.html', title=title, offers=offers, user=user)
+
+    if form.validate_on_submit():
+        picture_filename = None
+        if form.picture.data:
+            filename = secure_filename(form.picture.data.filename)
+            picture_filename = filename
+            form.picture.data.save(os.path.join('static/OffersPics', filename))
+
+        database.create_offer(
+            user.username,
+            form.port_departure.data,
+            form.port_arrival.data,
+            form.date_departure.data,
+            form.days.data,
+            form.pax.data,
+            form.price.data,
+            picture_filename
+        )
+        flash("New offer created!")
+        return redirect(url_for('feed'))
+
+    offers = database.get_offers()
+    return render_template("new.html", title=title, form=form, user=user, offers=offers)
 
 @app.route("/featured")
 @login_required
@@ -159,3 +203,17 @@ def featured():
 @app.route("/test")
 def test():
     return render_template('test.html', users=users)
+
+@app.route("/like/<int:offer_id>", methods=["POST"])
+@login_required
+def like_offer(offer_id):
+    user_id = database.get_user_id(current_user.username)
+    if database.user_liked_offer(user_id, offer_id):
+        database.unlike_offer(user_id, offer_id)
+        liked = False
+    else:
+        database.like_offer(user_id, offer_id)
+        liked = True
+
+    like_count = database.count_likes(offer_id)
+    return jsonify({"liked": liked, "likeCount": like_count})
--- database.py
+++ database.py
@@ -59,14 +59,115 @@ def get_client_by_username(Username):
     else:
         return None
 
-def create_user(first_name, last_name, username, dob, email, password_hash, bio):
-    return query('''INSERT INTO USERS
-                    ([Username],
-                    [FirstName],
-                    [LastName],
-                    [Email],
-                    [PasswordHash],
-                    [DoB],
-                    [Bio])
-                VALUES (?,?,?,?,?,?,?)''',
-                username, first_name, last_name, email, password_hash, dob, bio)
+def create_user(first_name, last_name, username, dob, email, password_hash, bio, profile_picture):
+    query('''
+        INSERT INTO USERS
+        ([Username], [FirstName], [LastName], [Email], [PasswordHash], [DoB], [Bio], [ProfilePicture])
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    ''', username, first_name, last_name, email, password_hash, dob, bio, profile_picture, fetch=False)
+
+
+def create_offer(username, port_departure, port_arrival, date_departure, days, pax, price, picture):
+    conn = sqlite3.connect('seayou.db')
+    cur = conn.cursor()
+
+    # Get UserId from username
+    cur.execute("SELECT UserId FROM Users WHERE Username = ?", (username,))
+    user_row = cur.fetchone()
+    if not user_row:
+        conn.close()
+        raise Exception("User not found.")
+
+    user_id = user_row[0]
+
+    # Insert offer
+    cur.execute('''
+        INSERT INTO Offers (
+            OfferDateTime,
+            UserId,
+            PortDeparture,
+            PortArrival,
+            DateDeparture,
+            Days,
+            Pax,
+            Price,
+            Picture
+        ) VALUES (
+            datetime('now'), ?, ?, ?, ?, ?, ?, ?, ?
+        )
+    ''', (user_id, port_departure, port_arrival, date_departure, days, pax, price, picture))
+
+    conn.commit()
+    conn.close()
+    
+def get_user_id(username):
+    conn = sqlite3.connect('seayou.db')
+    cur = conn.cursor()
+    cur.execute("SELECT UserId FROM Users WHERE Username = ?", (username,))
+    row = cur.fetchone()
+    conn.close()
+    return row[0] if row else None
+
+def user_liked_offer(user_id, offer_id):
+    conn = sqlite3.connect('seayou.db')
+    cur = conn.cursor()
+    cur.execute("SELECT 1 FROM Likes WHERE UserId = ? AND OfferId = ?", (user_id, offer_id))
+    result = cur.fetchone()
+    conn.close()
+    return result is not None
+
+def like_offer(user_id, offer_id):
+    conn = sqlite3.connect('seayou.db')
+    cur = conn.cursor()
+    cur.execute("INSERT INTO Likes (OfferId, UserId) VALUES (?, ?)", (offer_id, user_id))
+    conn.commit()
+    conn.close()
+
+def unlike_offer(user_id, offer_id):
+    conn = sqlite3.connect('seayou.db')
+    cur = conn.cursor()
+    cur.execute("DELETE FROM Likes WHERE OfferId = ? AND UserId = ?", (offer_id, user_id))
+    conn.commit()
+    conn.close()
+
+def get_offers_for_user(user_id):
+    conn = sqlite3.connect('seayou.db')
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT 
+            Offers.OfferId,
+            Offers.OfferDateTime,
+            Offers.PortDeparture,
+            Offers.DateDeparture,
+            Offers.Days,
+            Offers.Pax,
+            Offers.Price,
+            Offers.Picture,
+            Users.Username,
+            Users.FirstName,
+            Users.LastName,
+            L.LikeCount,
+            CASE WHEN UL.UserId IS NOT NULL THEN 1 ELSE 0 END AS LikedByUser
+        FROM Offers
+        INNER JOIN Users ON Offers.UserId = Users.UserId
+        LEFT JOIN (
+            SELECT OfferId, COUNT(UserId) AS LikeCount
+            FROM Likes
+            GROUP BY OfferId
+        ) L ON Offers.OfferId = L.OfferId
+        LEFT JOIN (
+            SELECT OfferId, UserId FROM Likes WHERE UserId = ?
+        ) UL ON Offers.OfferId = UL.OfferId
+    ''', (user_id,))
+    column_names = [desc[0] for desc in cur.description]
+    rows = cur.fetchall()
+    conn.close()
+    return [dict(zip(column_names, row)) for row in rows]
+
+def count_likes(offer_id):
+    conn = sqlite3.connect('seayou.db')
+    cur = conn.cursor()
+    cur.execute("SELECT COUNT(*) FROM Likes WHERE OfferId = ?", (offer_id,))
+    result = cur.fetchone()
+    conn.close()
+    return result[0] if result else 0
